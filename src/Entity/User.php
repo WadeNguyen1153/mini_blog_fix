99970391c5b05448b5a90abfbc70edd5a761b635
WadeNguyen1153 <wadenguyen1105@gmail.com>
2023-10-12 10:41:24 +0700

    improve category

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -165,4 +165,8 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+    public function __toString(): string
+    {
+        return $this->email;
+    }
 }
